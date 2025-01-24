02ce7ef02ca248dc5830be4e2dd4b4746042c032
Matthewwww1 <sarayut.so.67@ubu.ac.th>
2025-01-24 14:04:18 +0700

    สร้าง class แรกบน Github repo

---
+++ app.py
@@ -0,0 +1,12 @@
+class student(object):
+  student_count = 0
+
+def _new_(self):
+  print('Student._new_')
+  
+def __init__(self):
+  print('Student.__init__')
+  self.gpa = 4.0
+  
+def study_hard(self):
+  print('Sir, yes sir.')
